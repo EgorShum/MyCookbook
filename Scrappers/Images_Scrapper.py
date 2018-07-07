d90f9d98e6e657fa1ba5a22a71fb89e083c689ff
EgorShom <89135509130@mail.ru>
2018-07-07 13:56:14 +0700

    Adding my First program

--- Scrappers/Images_Scrapper.py
+++ Scrappers/Images_Scrapper.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup   # Для навигации по HTML
 import requests                 # Для отправки запросов на сайты
 from io import BytesIO          # Input output
 from PIL import Image           # Работа с картинками
-import os
+import os                       # Для проверки директорий и файлов
 
 def StartSearch():
     search = input("Search for: ")
     My_Params = {"q": search}
-    dir_name = search.replace(" ", "_").lower()
+    dir_name = search.replace(" ", "_").lower() # формируем имя директории куда будем сохранять картинки
 
-    if not os.path.isdir(dir_name):
+    if not os.path.isdir(dir_name): # Create dir if not exist
         os.makedirs(dir_name)
 
     r = requests.get("http://www.bing.com/images/search", params=My_Params)
@@ -19,10 +19,11 @@ def StartSearch():
     links = My_Soup.findAll("a", {"class": "thumb"})
 
     for item in links:
+        # в ходе отладки всплывали ошибки. Используем try дабы отловить их.
         try:
             img_obj = requests.get(item.attrs["href"])
             print("Getting", item.attrs["href"])
-            title = item.attrs["href"].split("/")[-1]
+            title = item.attrs["href"].split("/")[-1] # формируем название картинки
             try:
                 img = Image.open(BytesIO(img_obj.content))
                 img.save("./" + dir_name + "/" + title, img.format)
